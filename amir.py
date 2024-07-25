ce84e68dda70cb8a3139794c40d91b79a67ccba9
Amirreza-pixel <bavafaamirreza18@gmail.com>
2024-07-25 15:14:36 +0330

    Add files via upload

---
+++ amir.py
@@ -0,0 +1,15 @@
+i=0
+
+for i in [0,1,2,3,4]:
+
+    score = int(input("enter your numbers to be checked"))
+
+    
+    if score<10:
+      
+        print("you are failed")
+    else:
+     
+        print("you are passsed")
+      
+        i = i+1
